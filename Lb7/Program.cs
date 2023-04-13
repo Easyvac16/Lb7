b3ef7b0bad4d99c3bc711f3b5680e50d6b16d700
Easyvac16 <gdima9051@gmail.com>
2023-04-13 19:21:07 +0300

    Add project files.

---
+++ Lb7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lb7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Array arr = new Array(5);
+            arr.Show();
+
+            Array arr2 = new Array(3);
+            arr2.Show("This is array 2");
+
+        }
+    }
+}
---
+++ Lb7/cs1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb7
+{
+    interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+
+    class Array : IOutput
+    {
+        private int[] data;
+
+        public Array(int size)
+        {
+            data = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                data[i] = rand.Next(10);
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Array elements:");
+            foreach (int value in data)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine($"Array elements with info: {info}");
+            foreach (int value in data)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    internal class cs1
+    {
+    }
+}
